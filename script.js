81ef586141723fab92c7c1c5cda716adfaf13958
tasosbeast <66928134+tasosbeast@users.noreply.github.com>
2023-12-14 23:29:40 +0200

    Add element selection and event handling in script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 'use strict';
+
+// Selecting elements
+
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 10;
+document.querySelector('.guess').value = 23;
+console.log(document.querySelector('.guess').value);
+
+// Handling click events
+document.querySelector('.check').addEventListener('click', function () {});
