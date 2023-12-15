99becc99d2b3a05ec56bdb937be48a60116f33cb
tasosbeast <66928134+tasosbeast@users.noreply.github.com>
2023-12-15 17:52:37 +0200

    Update secret number display and correct number message

--- script.js
+++ script.js
@@ -8,7 +8,7 @@
 // console.log(document.querySelector('.guess').value);
 
 const secretNumber = Math.trunc(Math.random() * 20) + 1;
-document.querySelector('.number').textContent = secretNumber;
+
 let score = 20;
 let highscore = 0;
 
@@ -33,6 +33,7 @@ document.querySelector('.check').addEventListener('click', function () {
     document.querySelector('.message').textContent = '⛔ No number!';
   } else if (guess === secretNumber) {
     if (score > 0) {
+      document.querySelector('.number').textContent = secretNumber;
       document.querySelector('.message').textContent = '🎉 Correct Number!';
       document.querySelector('body').style.backgroundColor = '#60b347';
       document.querySelector('.number').style.width = '30rem';
